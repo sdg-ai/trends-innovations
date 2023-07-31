653e8431c3adba263efbc54cb897de5e6dafeeff
JohannesGetzner <j.getzner@gmail.com>
2023-07-31 15:18:50 +0200

    added a small frontend template to test the classifier

--- app/app.py
+++ app/app.py
@@ -2,26 +2,27 @@ import torch
 import os
 import uvicorn
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 from models.transformer import TransformerTandIClassifier
+from fastapi.templating import Jinja2Templates
+
+print(os.getcwd())
+templates = Jinja2Templates(directory="app/templates")
+
 logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"],
+    allow_headers=["*"])
 
 if os.path.exists(f"app/checkpoints/distilbert-base-uncased"):
     MODEL = TransformerTandIClassifier(model_name="distilbert-base-uncased",
-        # TODO: create a default config somewhere else instead of hard coding it here
-        model_config={"device": 'cuda' if torch.cuda.is_available() else 'cpu', "num_labels": 17})
+                                       # TODO: create a default config somewhere else instead of hard coding it here
+                                       model_config={"device": 'cuda' if torch.cuda.is_available() else 'cpu',
+                                                     "num_labels": 17})
 else:
     raise Exception("No model checkpoint found. Please train a model first.")
 
@@ -38,9 +39,22 @@ def predict(samples: List[Sample]) -> List[str]:
     :return: a list with a label for each sample
     """
     samples = [s.text for s in samples]
-    predictions = MODEL.predict(samples)
+    predictions, probs = MODEL.predict(samples)
     return predictions
 
 
+@app.get("/")
+def form_post(request: Request):
+    result = ""
+    return templates.TemplateResponse('app.html', context={'request': request, 'result': result})
+
+
+@app.post("/")
+def form_post(request: Request, text: str = Form(...)):
+    prediction, probs = MODEL.predict([text])
+    result = f"category: {prediction[0]} ({round(probs[0].item(), 2)})"
+    return templates.TemplateResponse('app.html', context={'request': request, 'result': result})
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, port=8000)

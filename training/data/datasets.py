8f459915f4d33a2c1f6649285d06e19eac05c7a4
JohannesGetzner <j.getzner@gmail.com>
2024-01-20 23:22:54 +0100

    added integration for new full dataset

--- training/data/datasets.py
+++ training/data/datasets.py
@@ -4,6 +4,7 @@ import json
 import wandb
 import torch
 import logging
+import itertools
 import numpy as np
 import pandas as pd
 from pickle import dump
@@ -100,6 +101,56 @@ def load_generated_data(data_dir: str, max_words_per_chunk:int = 65) -> pd.DataF
     return df
 
 
+def encode_labels(df, config):
+    le = preprocessing.LabelEncoder()
+    le.fit(df.label)
+    if not os.path.exists(config["save_model_dir"]):
+        os.makedirs(config["save_model_dir"])
+    dump(le, open(os.path.join(config["save_model_dir"], "label_encoder.pkl"), 'wb'))
+    return df, le
+
+def split_data_into_train_val_test(df, config):
+    # Convert percentage splits to absolute counts
+    train_size = int(config["dataset_splits"][0] * len(df))
+    val_size = int((config["dataset_splits"][1] - config["dataset_splits"][0]) * len(df))
+    test_size = len(df) - train_size - val_size
+    # Perform stratified splits
+    train_df, temp_df = train_test_split(df, test_size=val_size + test_size, stratify=df['label'], random_state=42)
+    val_df, test_df = train_test_split(temp_df, test_size=test_size, stratify=temp_df['label'], random_state=42)
+    return train_df, val_df, test_df
+
+
+def get_data_loaders_with_chatgpt_annotated_data(config, debug=False):
+    # load parquet file
+    df = pd.read_parquet(os.path.join(config["data_dir"], "chat_gpt_annotated_data.parquet"))
+    if debug:
+        # sample from every label to get a small dataset
+        dfs = []
+        for label in df.label.unique():
+            num_samples_for_label = len(df[df["label"] == label])
+            dfs.append(df[df["label"] == label].sample(min(10, num_samples_for_label)))
+        df = pd.concat(dfs)
+    # get encodings for labels
+    df, le = encode_labels(df, config)
+    # split into train, test, val
+    train_df, val_df, test_df = split_data_into_train_val_test(df, config)
+    train_df.reset_index(inplace=True, drop=True)
+    val_df.reset_index(inplace=True, drop=True)
+    test_df.reset_index(inplace=True, drop=True)
+    tokenizer = init_tokenizer(train_df, config)
+    logging.info(f"Size of train_df: {len(train_df)}")
+    logging.info(f"Size of val_df: {len(val_df)}")
+    logging.info(f"Size of test_df: {len(test_df)}")
+    datasets = {"train": DataLoader(TAndIDataSet(train_df, tokenizer, le), batch_size=config["batch_sizes"]["train"],
+                                    shuffle=True, generator=Generator().manual_seed(2147483647)),
+        "val": DataLoader(TAndIDataSet(val_df, tokenizer, le), batch_size=config["batch_sizes"]["val"], shuffle=True,
+                          generator=Generator().manual_seed(2147483647)),
+        "test": DataLoader(TAndIDataSet(test_df, tokenizer, le), batch_size=config["batch_sizes"]["test"], shuffle=True,
+                           generator=Generator().manual_seed(2147483647))}
+    tokenizer.save_pretrained(config['save_model_dir'])
+    return datasets, le
+
+
 def get_data_loaders(config, debug=False):
     """
     Given a data-path, a model, and the batch-sizes, return a ready to use dictionary of data loaders for training
@@ -109,6 +160,7 @@ def get_data_loaders(config, debug=False):
     """
     # load csv/json
     df = load_json_data(config["data_dir"])
+    # debug mode
     if debug:
         # sample from every label to get a small dataset
         dfs = []
@@ -124,19 +176,11 @@ def get_data_loaders(config, debug=False):
         os.makedirs(config["save_model_dir"])
     dump(le, open(os.path.join(config["save_model_dir"], "label_encoder.pkl"), 'wb'))
     # split into train, test, val
-    # Convert percentage splits to absolute counts
-    train_size = int(config["dataset_splits"][0] * len(df))
-    val_size = int((config["dataset_splits"][1] - config["dataset_splits"][0]) * len(df))
-    test_size = len(df) - train_size - val_size
-
-    # Perform stratified splits
-    train_df, temp_df = train_test_split(df, test_size=val_size + test_size, stratify=df['label'], random_state=42)
-    val_df, test_df = train_test_split(temp_df, test_size=test_size, stratify=temp_df['label'], random_state=42)
-
+    train_df, val_df, test_df = split_data_into_train_val_test(df, config)
     train_df.reset_index(inplace=True, drop=True)
     val_df.reset_index(inplace=True, drop=True)
     test_df.reset_index(inplace=True, drop=True)
-    tokenizer = AutoTokenizer.from_pretrained(config["model_name"], use_fast=False)
+    tokenizer = init_tokenizer(train_df, config)
     logging.info(f"Size of train_df: {len(train_df)}")
     logging.info(f"Size of val_df: {len(val_df)}")
     logging.info(f"Size of test_df: {len(test_df)}")
@@ -151,6 +195,14 @@ def get_data_loaders(config, debug=False):
     return datasets, le
 
 
+def init_tokenizer(train_df, config):
+    new_spans = set(itertools.chain.from_iterable(train_df.spans.tolist()))
+    tokenizer = AutoTokenizer.from_pretrained(config["model_name"], use_fast=True, max_len=512)
+    new_tokens = set(new_spans) - set(tokenizer.vocab.keys())
+    tokenizer.add_tokens(list(new_tokens))
+    return tokenizer
+
+
 def get_data_loaders_with_generated_data(config, debug=False):
     logging.info("Loading generated data.")
     df_real = load_json_data(config["data_dir"])
@@ -158,33 +210,29 @@ def get_data_loaders_with_generated_data(config, debug=False):
     df_generated = load_generated_data(config["data_dir"])
     df_generated["generated"] = True
     logging.info("The following categories contain generated data:", df_generated.label.unique())
-
-    le = preprocessing.LabelEncoder()
-    le.fit(df_real.label)
-    if not os.path.exists(config["save_model_dir"]):
-        os.makedirs(config["save_model_dir"])
-    dump(le, open(os.path.join(config["save_model_dir"], "label_encoder.pkl"), 'wb'))
-
+    df_real, le = encode_labels(df_real, config)
     # all the generated data goes into the training data set
     # and all the articles that data was generated too as well
     train_df_generated = df_generated
     train_df_real_generated_from = df_real[df_real["article_id"].isin(df_generated["article_id"].unique())]
     df_real_remaining = df_real[~df_real["article_id"].isin(df_generated["article_id"].unique())]
     # split the remaining real data into train, val, test
-    train_df, val_df, test_df = np.split(
-        df_real_remaining.sample(frac=1, random_state=42),
-        [int(config["dataset_splits"][0] * len(df_real_remaining)), int(config["dataset_splits"][1] * len(df_real_remaining))])
+    train_df, val_df, test_df = split_data_into_train_val_test(df_real_remaining, config)
     # add train_df_generated and train_df_real_generated_from to train_df
     train_df = pd.concat([train_df, train_df_generated, train_df_real_generated_from])
     if debug:
-        train_df = train_df.sample(100)
+        # sample from every label to get a small dataset
+        dfs = []
+        for label in train_df.label.unique():
+            dfs.append(train_df[train_df["label"] == label].sample(10))
+        train_df = pd.concat(dfs)
     logging.info(f"Size of train_df:{len(train_df)} with % generated data: {round(len(train_df[train_df['generated'] == True]) / len(train_df)*100, 2)}")
     logging.info(f"Size of val_df:{len(val_df)}", )
     logging.info(f"Size of test_df:{len(test_df)}", )
     train_df.reset_index(inplace=True, drop=True)
     val_df.reset_index(inplace=True, drop=True)
     test_df.reset_index(inplace=True, drop=True)
-    tokenizer = AutoTokenizer.from_pretrained(config["model_name"], use_fast=False)
+    tokenizer = init_tokenizer(train_df, config)
     datasets = {
         "train": DataLoader(TAndIDataSet(train_df, tokenizer, le), batch_size=config["batch_sizes"]["train"],
                                     shuffle=True, generator=Generator().manual_seed(2147483647)),

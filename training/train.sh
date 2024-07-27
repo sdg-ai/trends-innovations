c7a9052a58d6fb931096149d0bde1db081c11bb8
JohannesGetzner <j.getzner@gmail.com>
2024-07-27 18:57:47 +0200

    fixed bash scripts

---
+++ training/train.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ANACONDA_ENV="tandic-training"
+source activate $ANACONDA_ENV
+echo "Running script with Anaconda environment: $ANACONDA_ENV"
+
+date=$(date '+%Y-%m-%d %H:%M:%S')
+python training/train.py --d "${date}" 

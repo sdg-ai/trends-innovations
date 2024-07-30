485be083f23d84f2ff43d38e5fda0b89de5f43ec
Johannes Getzner <j.getzner@gmail.com>
2024-07-30 21:51:54 +0000

    adjusted configs

--- training/train.sh
+++ training/train.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-ANACONDA_ENV="tandic-training"
-source activate $ANACONDA_ENV
-echo "Running script with Anaconda environment: $ANACONDA_ENV"
 
+# Set the Anaconda environment
+ANACONDA_ENV="tandic-training"
+
+# Name of the tmux session
+TMUX_SESSION="training_session"
+
+# Start a new tmux session and run the commands
+tmux new-session -d -s $TMUX_SESSION
+
+# Activate the Anaconda environment and run the script inside the tmux session
+tmux send-keys -t $TMUX_SESSION "source activate $ANACONDA_ENV" C-m
+tmux send-keys -t $TMUX_SESSION "echo 'Running script with Anaconda environment: $ANACONDA_ENV'" C-m
+
+# Run the Python script with the current date
 date=$(date '+%Y-%m-%d %H:%M:%S')
-python training/train.py --d "${date}" 
+tmux send-keys -t $TMUX_SESSION "python training/train.py --d '${date}'" C-m
+
+# After the script is done, kill the tmux session
+tmux send-keys -t $TMUX_SESSION "tmux kill-session -t $TMUX_SESSION" C-m

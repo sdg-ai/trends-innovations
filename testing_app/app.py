6f5caf4d6d41d7b96b70f12cd6d421c0310fce8b
Johannes Getzner <johannes.getzner@elunic.com>
2024-08-01 01:33:17 +0200

    update gitignore

--- testing_app/app.py
+++ testing_app/app.py
@@ -2,37 +2,46 @@ import streamlit as st
 import torch
 from transformers import DistilBertForSequenceClassification, AutoTokenizer
 from torch.nn.functional import softmax
+import os
+import joblib
 
-
-MODEL_DIR = '/checkpoint/2024-07-30 22:10:25-distilbert-base-uncased/seed1'
-TOKENIZER_DIR = '/checkpoint/2024-07-30 22:10:25-distilbert-base-uncased'
+print(os.getcwd())
+MODEL_DIR = './checkpoint/2024-07-30 22:10:25-distilbert-base-uncased/seed_1/'
+TOKENIZER_DIR = './checkpoint/2024-07-30 22:10:25-distilbert-base-uncased'
 
 
 def load_model():
     model = DistilBertForSequenceClassification.from_pretrained(MODEL_DIR, local_files_only=True)
     tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_DIR, local_files_only=True)
-    model.eval()  
-    return model, tokenizer
+    label_encoder = joblib.load(f"{TOKENIZER_DIR}/label_encoder.pkl") 
+    model.eval()
+    return model, tokenizer, label_encoder
 
-
-def predict(text, model, tokenizer):
+def predict(text, model, tokenizer, label_encoder):
     inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True, max_length=512)
     with torch.no_grad():
         outputs = model(**inputs)
-    probabilities = softmax(outputs.logits, dim=1)
-    return probabilities
+    probabilities = softmax(outputs.logits, dim=1).squeeze(0)
+    # Convert logits to class names with probabilities
+    results = [(label_encoder.inverse_transform([i])[0], prob.item()) for i, prob in enumerate(probabilities)]
+    # Sort results by probability in descending order
+    sorted_results = sorted(results, key=lambda x: x[1], reverse=True)
+    return sorted_results
 
 
-model, tokenizer = load_model()
+model, tokenizer, label_encoder = load_model()
 
 
 st.title("TandI-Classifier")
 user_input = st.text_input("Enter text for classification:")
 if st.button("Submit"):
     if user_input:
-        probabilities = predict(user_input, model, tokenizer)
+        sorted_results = predict(user_input, model, tokenizer, label_encoder)
         st.write("Classification probabilities:")
-        for idx, prob in enumerate(probabilities.squeeze(0)):
-            st.write(f"Class {idx}: {prob.item():.6f}")
+        for name, prob in sorted_results:
+            if prob == sorted_results[0][1]: 
+                st.markdown(f"**{name}: {prob:.6f}**", unsafe_allow_html=True)  
+            else:
+                st.write(f"{name}: {prob:.6f}")
     else:
         st.error("Please enter some text to classify.")
